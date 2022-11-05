b789767ae50c5392880cc961e10645732fabea59
mohamed nour_eldien <mohamednour20191700@gmail.com>
2022-11-05 19:25:46 +0200

    adding gui
    adding gui

--- Single Layer Perceptron/main.py
+++ Single Layer Perceptron/main.py
@@ -3,6 +3,11 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from Perceptron import Perceptron
+import pandas as pd
+from tkinter import *
+from tkinter import messagebox
+
+
 
 # Handle dataset and create a map for the Features and Classes
 dataset = pd.read_csv('processed-penguins.csv')
@@ -49,45 +54,149 @@ def accuracy(y_true, y_predict):
     acccuray= np.sum(y_true==y_predict)/ len(y_true)
     return acccuray
 
-# taking classes and features from user
-c1 = input("enter class number: ")
-c2 = input("enter class number: ")
-train, test = CreateTrainAndTestDataframes(int(c1),int(c2))
-f1 = input("enter feature number: ")
-f2 = input("enter feature number: ")
-X_train, Y_train,X_test, Y_test= SelectFeatures(int(f1),int(2),train,test)
-
-# creating perceptron object
-p = Perceptron(1,2)
-p.fit(X_train,Y_train,Species)
-predictions = p.predict(X_test)
-
-# testing and calculating accuracy
-y_converted = Y_test.to_numpy()
-y=np.array([1 if i==Species[1] else -1 for i in y_converted])
-print("aacuracy: ", accuracy(y,predictions))
 
 
-# Plotting the graph
-xtrain = X_train.to_numpy()
-y_converted = Y_train.to_numpy()
-y = np.array([1 if i == Species[1] else -1 for i in y_converted])
+#creating form and put its dimension
+form = Tk()
+form.geometry("800x500")
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-plt.scatter(xtrain[:, 0], xtrain[:, 1], marker='o', c=y)
+#label for classes
+var = StringVar()
+label1 = Label( form, textvariable=var )
+var.set("Select Two Classes:")
+label1.pack(anchor = W )
 
-x01 = np.amin(xtrain[:, 0])
-x02 = np.amax(xtrain[:, 0])
+#radio button for classes
+classesNumbers = IntVar()
+R1 = Radiobutton(form, text="Adelie & Gentoo", variable=classesNumbers, value=1)
+R1.pack( anchor = W )
 
-x11 = (-p.weights[0] * x01 - p.bias) / p.weights[1]
-x12 = (-p.weights[0] * x02 - p.bias) / p.weights[1]
+R2 = Radiobutton(form, text="Adelie & Chinstrap", variable=classesNumbers, value=2)
+R2.pack( anchor = W )
 
-ax.plot([x01, x02], [x11, x12], 'k')
+R3 = Radiobutton(form, text="Gentoo & Chinstrap", variable=classesNumbers, value=3)
+R3.pack( anchor = W)
 
-ymin = np.amin(xtrain[:, 1])
-ymax = np.amax(xtrain[:, 1])
 
-ax.set_ylim([ymin - 3, ymax + 3])
+#label for features
+var = StringVar()
+label2 = Label( form, textvariable=var)
+var.set("Select Two Features:")
+label2.pack(anchor = W )
 
-plt.show()
+#check boxes for features
+features=[IntVar(),IntVar(),IntVar(),IntVar(),IntVar()]
+selectedFeatures=[]
+C1 = Checkbutton(form, text = "bill_length_mm", variable = features[0],onvalue = 1, offvalue = 0)
+C2 = Checkbutton(form, text = "bill_depth_mm", variable = features[1],onvalue = 2, offvalue = 0)
+C3 = Checkbutton(form, text = "flipper_length_mm", variable = features[2],onvalue = 3, offvalue = 0)
+C4 = Checkbutton(form, text = "gender", variable = features[3],onvalue = 4, offvalue = 0)
+C5 = Checkbutton(form, text = "body_mass_g", variable = features[4],onvalue = 5, offvalue = 0)
+C1.pack(anchor = W)
+C2.pack(anchor = W)
+C3.pack(anchor = W)
+C4.pack(anchor = W)
+C5.pack(anchor = W)
+
+
+#label for learning rate
+var = StringVar()
+label3 = Label( form, textvariable=var,)
+var.set("Enter Learning Rate(eta):")
+label3.pack(anchor = W )
+
+#textBox for learning rate
+learningRate = DoubleVar()
+E1 = Entry(form, bd =5,textvariable=learningRate)
+E1.pack(anchor=W)
+
+#label for epochs
+var = StringVar()
+label4 = Label( form, textvariable=var)
+var.set("Number Of Epochs(m):")
+label4.pack(anchor = W )
+
+#textBox for epochs
+numberOfEpochs = IntVar()
+E2 = Entry(form, bd =5,textvariable=numberOfEpochs)
+E2.pack(anchor=W)
+
+#checkbox for bias
+isBias=IntVar()
+C6 = Checkbutton(form, text = "bias", variable = isBias,onvalue = 1, offvalue = 0)
+C6.pack(anchor = W)
+
+
+def startPrediction():
+        # taking classes and features from user
+    if(classesNumbers.get()==1):
+        c1=1
+        c2=2
+    elif(classesNumbers.get()==2):
+        c1=1
+        c2=3
+    else:
+        c1=2
+        c2=3
+
+
+    train, test = CreateTrainAndTestDataframes(int(c1),int(c2))
+    selectFeaures=[]
+    for i in range(5):
+        if(features[i].get()!=0):
+            selectFeaures.append(i)
+    f1=selectFeaures[0]+1
+    f2=selectFeaures[1]+1
+
+    print("c1: " + str(c1))
+    print("c2: " + str(c2))
+    print("f1: " + str(f1))
+    print("f2: " + str(f2))
+    print("learning rate: " + str(learningRate.get()))
+    print("epochs: "+ str(numberOfEpochs.get()))
+    print("bias: "+str(isBias.get()==1))
+
+    X_train, Y_train,X_test, Y_test= SelectFeatures(int(f1),int(2),train,test)
+
+    # creating perceptron object
+    p = Perceptron(c1,c2,learningRate.get(),numberOfEpochs.get(),isBias.get()==1)
+    p.fit(X_train,Y_train,Species)
+    predictions = p.predict(X_test)
+
+    # testing and calculating accuracy
+    y_converted = Y_test.to_numpy()
+    y=np.array([1 if i==Species[1] else -1 for i in y_converted])
+    print("aacuracy: ", accuracy(y,predictions))
+    messagebox.showinfo("Accuracy", "Accuracy Is: " + str(accuracy(y, predictions)))
+
+    # Plotting the graph
+    xtrain = X_train.to_numpy()
+    y_converted = Y_train.to_numpy()
+    y = np.array([1 if i == Species[1] else -1 for i in y_converted])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(xtrain[:, 0], xtrain[:, 1], marker='o', c=y)
+
+    x01 = np.amin(xtrain[:, 0])
+    x02 = np.amax(xtrain[:, 0])
+
+    x11 = (-p.weights[0] * x01 - p.bias) / p.weights[1]
+    x12 = (-p.weights[0] * x02 - p.bias) / p.weights[1]
+
+    ax.plot([x01, x02], [x11, x12], 'k')
+
+    ymin = np.amin(xtrain[:, 1])
+    ymax = np.amax(xtrain[:, 1])
+
+    ax.set_ylim([ymin - 3, ymax + 3])
+
+    plt.show()
+
+
+#start Button
+button=Button(form,text="Start",width=10,command=startPrediction)
+button.pack()
+
+#main loop
+form.mainloop()
